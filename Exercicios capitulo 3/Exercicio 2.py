4ed3ff6ec70cb06ab04ad3d142a1d527a88e5361
Pedro Henrique Ribeiro Dias <125469206+zSh3rl0cK@users.noreply.github.com>
2025-08-19 16:03:25 -0300

    Delete Exercicios capitulo 3 directory

--- Exercicios capitulo 3/Exercicio 2.py
+++
@@ -1,9 +0,0 @@
-estoqueLoja1 = {'Samsung', 'Nokia', 'Motorola', 'Iphone'}
-
-estoqueLoja2 = {'Samsung', 'Xiaomi', 'Iphone', 'LG'}
-
-estoqueGeral = estoqueLoja1.union(estoqueLoja2)
-
-print("Modelos disponiveis no total: ", estoqueGeral)
-print("Modelos Loja 1: ", estoqueLoja1)
-print("Modelos Loja 2: ", estoqueLoja2)
