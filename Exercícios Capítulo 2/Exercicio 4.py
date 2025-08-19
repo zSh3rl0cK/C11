a7b3e7dbeabf293df8e471d94916ea2f5a53d268
Pedro Henrique Ribeiro Dias <125469206+zSh3rl0cK@users.noreply.github.com>
2025-08-19 16:03:09 -0300

    Delete Exercícios Capítulo 2 directory

--- Exercícios Capítulo 2/Exercicio 4.py
+++
@@ -1,9 +0,0 @@
-kilometros = float(input("Digite a distancia da sua viagem"))
-if kilometros <= 200:
-    preco = kilometros*0.50
-    print("O preço a ser pago é: ", preco)
-
-else:
-    preco = kilometros*0.45
-    print("O preço a ser pago é: ", preco)
-    
--- Exercícios Capítulo 2/Exercício 2.py
+++
@@ -1,7 +0,0 @@
-numero = float(input("Digite um número "))
-
-intervalo1 = int(input("Digite o inicio do intervalo "))
-intervalo2 = int(input("Digite o final do intervalo"))
-
-for c in range(intervalo1, intervalo2):
-    print(c*numero)
