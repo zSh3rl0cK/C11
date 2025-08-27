cb1d21fee335a8173a06fc5f907f751b68a9789b
zSh3rl0cK <pedrohrdias12@gmail.com>
2025-08-27 11:29:04 -0300

    Exercícios capitulo 4 parte 3

---
+++ Exercícios/ExerciciosCap4P3/Exercícios.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+ds = np.loadtxt("space.csv",
+                delimiter=";",
+                dtype="str",
+                encoding="utf-8") # especificamente esse dataset é utf 8
+
+# Questão 1
+mtz = ds[1:, 7] # Matriz formada pelos status da missao pulando a linha do cabeçalho
+print(f"A porcentagem de sucessos é: {np.round(((np.sum(mtz == "Success"))/len(mtz))*100,4)}% \n")
+
+# Questão 2
+mtz = ds[1:, 6].astype(float) # Matriz formada pelas linhas com o custo de cada missão
+print(f"A média de custo para missões com valores (>0) é: {np.round(np.sum(mtz[mtz > 0])/(np.sum(mtz > 0)),2)} \n")
+
+# Questão 3
+mtz = ds[1:, 2] # Matriz formada pelas linhas dos locais de cada missão
+print(f"Missões realizadas pelos EUA: {np.sum(np.char.find(mtz, 'USA') >= 0)} \n") # poderia usar apenas count tbm
+
+# Questão 4
+mtz = (ds[1:, 1] == "SpaceX")
+custo = ds[1:, 6].astype(float)
+
+max_custo = np.max(custo[mtz])  # valor máximo de custo
+mask_max = (custo == max_custo) & mtz  # missões da SpaceX que têm esse custo máximo
+
+print(f"As missões mais caras da empresa SpaceX custaram: {max_custo} milhões \n")
+for linha in ds[1:][mask_max]:
+    print(linha)
+
+# Questão 5
+empresas = np.unique(ds[1:, 1])   # nomes únicos
+for empresa in empresas:
+    print(f"A empresa {empresa} realizou {np.sum(ds[1:, 1]  == empresa)} missões")
